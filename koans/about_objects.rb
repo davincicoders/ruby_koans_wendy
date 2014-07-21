afba1603d5765e26a0028ea71524fb348f0905b8
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-07-21 12:53:24 -0600

    Make test_every_object_has_an_id tests pass
      * entered true value for the the assert_equal method
    
      * the objects' ids shouldn't equal each other
    
      [#75305762]

--- koans/about_objects.rb
+++ koans/about_objects.rb
@@ -21,7 +21,11 @@ class AboutObjects < Neo::Koan
 
   def test_every_object_has_an_id
     obj = Object.new
+<<<<<<< HEAD
     assert_equal Fixnum, obj.object_id.class
+=======
+    assert_equal Fixum, obj.object_id.class
+>>>>>>> Make test_every_object_has_an_id tests pass
   end
 
   def test_every_object_has_different_id
