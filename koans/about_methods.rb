446698e7e2f7eed5e377903d40dae0e255412f16
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-07-27 16:46:18 -0600

    Make the following tests pass…
      - test_calling_methods_in_same_class
      - test_calling_methods_in_same_class_with_explicit_receiver
      -  test_calling_private_methods_without_receiver
      -  test_calling_private_methods_with_an_explicit_receiver
    
      * entered the argument values for the the assert_equal methods
    
      * the tests now have two values to compare
    
    [#75305776]

--- koans/about_methods.rb
+++ koans/about_methods.rb
@@ -98,11 +98,11 @@ class AboutMethods < Neo::Koan
   end
 
   def test_calling_methods_in_same_class
-    assert_equal __, my_method_in_the_same_class(3,4)
+    assert_equal 12, my_method_in_the_same_class(3,4)
   end
 
   def test_calling_methods_in_same_class_with_explicit_receiver
-    assert_equal __, self.my_method_in_the_same_class(3,4)
+    assert_equal 12, self.my_method_in_the_same_class(3,4)
   end
 
   # ------------------------------------------------------------------
@@ -113,14 +113,14 @@ class AboutMethods < Neo::Koan
   private :my_private_method
 
   def test_calling_private_methods_without_receiver
-    assert_equal __, my_private_method
+    assert_equal "a secret", my_private_method
   end
 
   def test_calling_private_methods_with_an_explicit_receiver
-    exception = assert_raise(___) do
+    exception = assert_raise(NoMethodError) do
       self.my_private_method
     end
-    assert_match /__/, exception.message
+    assert_match /\Aprivate/, exception.message
   end
 
   # ------------------------------------------------------------------
